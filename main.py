6848b779570f8602c734d2043a9bc8ba19857789
Gakmlr7896 <mohamedwesam772@outlook.sa>
2025-11-24 22:34:32 +0300

    sdfasdfgggg

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import torch 
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+import pandas as pd
+import tensorboard as tb
 
-TENSOR = torch.Tensor([[8,3,4],[1,6,7]])
-print(TENSOR)
+# Load dataset
+iris = datasets.load_
